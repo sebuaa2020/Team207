fcdb025e36b805ef911eba6dc8ab0a42dd7ad862
王念 <17373183@buaa.edu.cn>
2020-05-29 07:47:59 +0800

    用户APP设计

---
+++ 代码/APP/app/src/main/java/com/example/roscontrol/CtrlWin.java
@@ -0,0 +1,16 @@
+package com.example.roscontrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class CtrlWin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ctrl_win);
+        //Intent intent = getIntent();
+    }
+}
